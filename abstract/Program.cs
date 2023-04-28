c81cda3b07f644fb3d1b40a5d172cbf6bf5d152b
Ethan Spencer <ethan101@byui.edu>
2023-04-27 22:15:40 -0600

    week2

---
+++ abstract/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Book mistborne = new Book("Mistborne","Brandon Sanderson");
+Book themartian = new Book("The Martian","Andy Weir");
+mistborne.Display();
+// Console.WriteLine(mistborne.IsAvailable());
+themartian.Display();
+// Console.WriteLine(mistborne.IsAvailable());
+
